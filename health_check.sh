e7110d39af3798943d5641d4f12246b9972a6b20
Helena Oliveira <helena_oliveirasilva@yahoo.com.br>
2026-03-13 18:19:14 -0300

    feat: adiciona verificacao de porta aberta com ss

--- health_check.sh
+++ health_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-URL="http://localhost:80"
+PORT=80
+URL="http://localhost:$PORT"
 LOG_FILE="health_check.log"
 SERVICE="apache2"
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
@@ -12,7 +12,10 @@ SERVICE_STATUS=$(systemctl is-active $SERVICE)
 # Verificar HTTP
 HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" $URL)
 
-if [ "$SERVICE_STATUS" = "active" ] && [ "$HTTP_CODE" -eq 200 ]; then
+# Verificar porta
+PORT_STATUS=$(ss -tunl | grep "$PORT" )
+
+if [ "$SERVICE_STATUS" = "active" ] && [ "$HTTP_CODE" -eq 200 ] && [ -n "$PORT_STATUS" ]; then
     STATUS="OK"
     EXIT_CODE=0
 else
